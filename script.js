b368a5034b8baf2f24e316408d2ad7dd126ea45d
Ahmed-GAMAL-Elhalaby <127055604+Ahmed-GAMAL-Elhalaby@users.noreply.github.com>
2023-06-14 04:22:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,40 @@
-document.getElementById('calorie-form').addEventListener('submit', function(e){
-  document.getElementById('results').style.display = 'none';
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
 
-  document.getElementById('loading').style.display = 'block';
-
-  setTimeout(calculateCalories, 2000);
-
-  e.preventDefault();
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue +=1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
 });
 
-function calculateCalories(e) {
-  
-  const age = document.getElementById('age');
-  const gender = document.querySelector('input[name="customRadioInline1"]:checked');
-  const weight = document.getElementById('weight');
-  const height = document.getElementById('height');
-  const activity = document.getElementById('list').value;
-  const totalCalories = document.getElementById('total-calories');
-  
-  
-  if (age.value === '' || weight.value === '' || height.value === '' || 80 < age.value || age.value < 15) {
-    errorMessage('Please make sure the values you entered are correct')
-  } else if(gender.id === 'male' && activity === "1") {
-    totalCalories.value = 1.2 * (66.5 + (13.75 * parseFloat(weight.value)) + (5.003 * parseFloat(height.value)) - (6.755 * parseFloat(age.value)));
-  } else if(gender.id === 'male' && activity === "2") {
-    totalCalories.value = 1.375 * (66.5 + (13.75 * parseFloat(weight.value)) + (5.003 * parseFloat(height.value)) - (6.755 * parseFloat(age.value)));
-  } else if (gender.id === 'male' && activity === "3") {
-    totalCalories.value = 1.55 * (66.5 + (13.75 * parseFloat(weight.value)) + (5.003 * parseFloat(height.value)) - (6.755 * parseFloat(age.value)));
-  } else if(gender.id === 'male' && activity === "4") {
-    totalCalories.value = 1.725 * (66.5 + (13.75 * parseFloat(weight.value)) + (5.003 * parseFloat(height.value)) - (6.755 * parseFloat(age.value)));
-  } else if(gender.id === 'male' && activity === "5") {
-    totalCalories.value = 1.9 * (66.5 + (13.75 * parseFloat(weight.value)) + (5.003 * parseFloat(height.value)) - (6.755 * parseFloat(age.value)))
-    ;
-  } else if(gender.id === 'female' && activity === "1") {
-    totalCalories.value = 1.2 * (655 + (9.563 * parseFloat(weight.value)) + (1.850 * parseFloat(height.value)) - (4.676 * parseFloat(age.value)));
-  } else if(gender.id === 'female' && activity === "2") {
-    totalCalories.value = 1.375 * (655 + (9.563 * parseFloat(weight.value)) + (1.850 * parseFloat(height.value)) - (4.676 * parseFloat(age.value)));
-  } else if(gender.id === 'female' && activity === "3") {
-    totalCalories.value = 1.55 * (655 + (9.563 * parseFloat(weight.value)) + (1.850 * parseFloat(height.value)) - (4.676 * parseFloat(age.value)));
-  } else if(gender.id === 'female' && activity === "4") {
-    totalCalories.value = 1.725* (655 + (9.563 * parseFloat(weight.value)) + (1.850 * parseFloat(height.value)) - (4.676 * parseFloat(age.value)));
-  } else {
-    totalCalories.value = 1.9 * (655 + (9.563 * parseFloat(weight.value)) + (1.850 * parseFloat(height)) - (4.676 * parseFloat(age.value)));
-  } 
+ // Select all elements with the "i" tag and store them in a NodeList called "stars"
+ const stars = document.querySelectorAll(".stars i");
+ // Loop through the "stars" NodeList
+ stars.forEach((star, index1) => {
+   // Add an event listener that runs a function when the "click" event is triggered
+   star.addEventListener("click", () => {
+     // Loop through the "stars" NodeList Again
+     stars.forEach((star, index2) => {
+       // Add the "active" class to the clicked star and any stars with a lower index
+       // and remove the "active" class from any stars with a higher index
+       index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+     });
+   });
+ });
 
-  document.getElementById('results').style.display = 'block';
 
-  document.getElementById('loading').style.display = 'none';
-}
 
-function errorMessage(error) {
-  document.getElementById('results').style.display = 'none';
+ const wrapper = document.querySelector('.menu__wrapper');
 
-  document.getElementById('loading').style.display = 'none';
-  const errorDiv = document.createElement('div');
-  const card = document.querySelector('.card');
-  const heading = document.querySelector('.heading');
-  errorDiv.className = 'alert alert-danger';
-  errorDiv.appendChild(document.createTextNode(error));
+ wrapper.addEventListener('click', () => {
+   wrapper.classList.toggle('active');
+ })
 
-  card.insertBefore(errorDiv, heading);
-
-  setTimeout(clearError, 4000);
-}
-
-function clearError() {
-  document.querySelector('.alert').remove();
-}
 
